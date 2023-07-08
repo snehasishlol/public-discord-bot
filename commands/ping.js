e7c55144c5f5e63a598b3b13d71d303682f0410a
snehasish's stuff <snehasishcodes@gmail.com>
2023-07-08 15:28:04 +0530

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,11 @@
 module.exports = {
-	exec: async(message, client, args) => {
-		message.reply({
-			content: `Pong! **${client.ws.ping}ms**`
-		});
-	},
+	name: 'ping',
+	description: "Check bot's ping.",
+	cooldown: 3000,
+	userPerms: [],
+	botPerms: [],
+	run: async (client, message, args) => {
+		const msg = await message.reply('Pinging...')
+		await msg.edit(`Pong! **${client.ws.ping} ms**`)
+	}
 };
